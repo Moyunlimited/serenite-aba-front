f3148b896a55f5890bba11ad7a9b53b596d2b071
Moyunlimited <moy.love@hotmail.com>
2025-07-27 18:39:14 -0400

    fin

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 const API_BASE =
   import.meta.env.MODE === "development"
-    ? "http://localhost:5000"
-    : "https://serenite-aba.onrender.com";
+    ? "http://localhost:5000/"
+    : "https://serenite-aba.onrender.com/";
 
 export default API_BASE;
 
